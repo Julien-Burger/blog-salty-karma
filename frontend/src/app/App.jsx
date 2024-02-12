0a95eeb7d7b57f4b7d32031cfeadc3dc299b76b6
Julien Burger <julienburger1998@gmail.com>
2024-02-12 19:47:42 +0100

    adding error page

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "../pages/home";
 import Blog from "../pages/blog";
+import Error from "../pages/error";
 import Header from "../components/header";
 import Footer from "../components/footer";
 
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/blog/:id" element={<Blog />} />
+                <Route path="*" element={<Error />} />
             </Routes>
             <Footer />
         </Router>
