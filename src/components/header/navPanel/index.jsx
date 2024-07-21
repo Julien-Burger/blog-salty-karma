65ee45bce6e3b804273c0633dbc60e1c234fe7ee
Julien-Burger <julienburger1998@gmail.com>
2024-07-21 21:17:22 +0200

    changing nav panel link

--- src/components/header/navPanel/index.jsx
+++ src/components/header/navPanel/index.jsx
@@ -1,30 +1,43 @@
-import { Link } from "react-router-dom";
-
 import "./style.scss";
 
 function NavPanel({ showNavPanel }) {
     return (
         <nav>
             <ul style={showNavPanel ? { display: "flex" } : {}}>
+                {/* <li>
+                    <a href="https://saltykarma.com">
+                        <i class="fa-solid fa-house"></i>Salty Karma
+                    </a>
+                </li> */}
                 <li>
-                    <a href="https://www.saltykarma.com">
-                        <i className="fa-solid fa-house"></i>
-                        Salty Karma
+                    <a
+                        href="https://store.steampowered.com/app/2232680/The_Big_One/"
+                        target="_blank"
+                        aria-label="Buy The Big One on Steam"
+                    >
+                        <i className="fa-brands fa-steam"></i>
+                        Steam
                     </a>
                 </li>
                 <li>
-                    <Link to="/" reloadDocument>
-                        <i className="fa-regular fa-newspaper"></i>Blog
-                    </Link>
-                </li>
-                <li>
-                    <a href="https://portfolio.saltykarma.com/">
-                        <i className="fa-solid fa-address-card"></i>Portfolio
+                    <a href="https://www.youtube.com/@saltykarma" target="_blank" aria-label="Subscribe to the Youtube channel">
+                        <i className="fa-brands fa-youtube"></i>Youtube
                     </a>
                 </li>
                 <li>
-                    <a href="https://github.com/Salty-Julien" target="_blank">
-                        <i className="fa-brands fa-github"></i>Github
+                    <a href="https://discord.com/invite/rtXFfFPsPw" target="_blank" aria-label="Join the Discord community">
+                        <i className="fa-brands fa-discord"></i>
+                        Discord
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.tiktok.com/@salty_karma" target="_blank" aria-label="Follow us on TikTok">
+                        <i class="fa-brands fa-tiktok"></i>TikTok
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/salty_karma_studio/" target="_blank" aria-label="Follow us on Instagram">
+                        <i className="fa-brands fa-instagram fa-lg"></i>Instagram
                     </a>
                 </li>
             </ul>
